9beec243fbb8fa3a874888895c57ca42ea6d9182
edutel <15623704+EduTel@users.noreply.github.com>
2025-03-14 22:53:10 -0600

    fix: profile 2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -15,49 +15,68 @@ import { handlePressLink } from "@/utils/linking";
 const { width } = Dimensions.get("window");
 
 export default function About() {
+  const theme = useTheme();
+
   return (
     <SafeAreaView>
-      <View style={styles.container}>
-        <Image
-          source={require("@/assets/images/profile.jpeg")}
-          style={styles.headerImage}
-        />
-      </View>
-      <ScrollView>
-        <View style={{ gap: 5 }}>
-          <Button
-            icon="microsoft-internet-explorer"
-            mode="text"
-            onPress={() => handlePressLink(profile?.url)}
-          >
-            {profile?.url.split(".com")[0]}
-          </Button>
-          <Text variant="headlineSmall">{profile.name}</Text>
-          <Text variant="headlineSmall">{profile.phone}</Text>
-          <View style={{ flexDirection: "row", alignItems: "center", gap: 5 }}>
-            <Entypo name="email" size={24} color="black" />
-            <Text variant="headlineSmall">{profile.email}</Text>
-          </View>
-          <View style={{ flexDirection: "row", alignItems: "center", gap: 5 }}>
-            <FontAwesome name="birthday-cake" size={24} color="black" />
-            <Text variant="headlineSmall">{profile.yearOfBirth}</Text>
-          </View>
+      <View style={{ paddingHorizontal: 20 }}>
+        <View style={styles.container}>
+          <Image
+            source={require("@/assets/images/profile.jpeg")}
+            style={styles.headerImage}
+          />
         </View>
-      </ScrollView>
+        <ScrollView>
+          <View style={{ gap: 5 }}>
+            <Button
+              icon="microsoft-internet-explorer"
+              mode="text"
+              onPress={() => handlePressLink(profile?.url)}
+            >
+              {profile?.url.split(".com")[0]}
+            </Button>
+            <Text theme={theme} variant="headlineSmall">
+              {profile.name}
+            </Text>
+            <Text theme={theme} variant="headlineSmall">
+              {profile.phone}
+            </Text>
+            <View
+              style={{ flexDirection: "row", alignItems: "center", gap: 5 }}
+            >
+              <Entypo name="email" size={24} color={theme.colors.onSurface} />
+              <Text theme={theme} variant="headlineSmall">
+                {profile.email}
+              </Text>
+            </View>
+            <View
+              style={{ flexDirection: "row", alignItems: "center", gap: 5 }}
+            >
+              <FontAwesome
+                name="birthday-cake"
+                size={24}
+                color={theme.colors.onSurface}
+              />
+              <Text theme={theme} variant="headlineSmall">
+                {profile.yearOfBirth}
+              </Text>
+            </View>
+          </View>
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    height: 200,
     overflow: "hidden",
+    alignItems: "center",
   },
   headerImage: {
-    width: width,
-    height: width,
+    borderRadius: 300,
+    width: 200,
+    height: 200,
     resizeMode: "cover",
-    position: "absolute",
-    top: 0,
   },
 });
